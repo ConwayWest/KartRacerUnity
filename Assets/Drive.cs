89f823861422576c8d57703dfaff716957716d2a
Kyle Conway <kyleconway@rocketmail.com>
2019-10-22 00:36:06 -0400

    Car Control & Basic AI
    Basic controls and AI (rough still)

--- Assets/Drive.cs
+++ Assets/Drive.cs
@@ -4,33 +4,163 @@ using UnityEngine;
 
 public class Drive : MonoBehaviour
 {
-    public WheelCollider WC;
+    public WheelCollider[] WCs;
+    public GameObject[] Wheels;
     public float torque = 200;
+    public float maxSteerAngle = 30;
+    public float maxBrakeTorque = 2000;
+    public float maxSpeed = 200;
+
+    public AudioSource skidSound;
+    public AudioSource highAccel;
+
+    public Transform SkidTrailPrefab;
+    Transform[] skidTrails = new Transform[4];
+    public ParticleSystem smokePrefab;
+    ParticleSystem[] skidSmoke = new ParticleSystem[4];
+
+    public GameObject brakeLightRight;
+    public GameObject brakeLightLeft;
+
+    public Rigidbody rb;
+    public float gearLength = 3;
+    public float currentSpeed { get { return rb.velocity.magnitude * gearLength; } }
+    public float lowPitch = 1f;
+    public float highPitch = 6f;
+    public int numGears = 5;
+    float rpm;
+    int currentGear = 1;
+    float currentGearPerc;
+
+    public void StartSkidTrail(int i)
+    {
+        if(skidTrails[i] == null)
+            skidTrails[i] = Instantiate(SkidTrailPrefab);
+
+        skidTrails[i].parent = WCs[i].transform;
+        skidTrails[i].localRotation = Quaternion.Euler(90, 0, 0);
+        skidTrails[i].localPosition = -Vector3.up * WCs[i].radius;
+    }
+
+    public void EndSkidTrail(int i)
+    {
+        if (skidTrails[i] == null) return;
+        Transform holder = skidTrails[i];
+        skidTrails[i] = null;
+        holder.parent = null;
+        holder.rotation = Quaternion.Euler(90, 0, 0);
+        Destroy(holder.gameObject, 30);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        WC = this.GetComponent<WheelCollider>();
+        for(int i = 0; i < 4; i++)
+        {
+            skidSmoke[i] = Instantiate(smokePrefab);
+            skidSmoke[i].Stop();
+        }
+
+        brakeLightRight.SetActive(false);
+        brakeLightLeft.SetActive(false);
     }
 
-    void Go(float accel)
+    public void CalculateEngineSound()
+    {
+        float gearPercentage = (1 / (float)numGears);
+        float targetGearFactor = Mathf.InverseLerp(gearPercentage * currentGear, gearPercentage * (currentGear + 1),
+                                                    Mathf.Abs(currentSpeed / maxSpeed));
+
+        currentGearPerc = Mathf.Lerp(currentGearPerc, targetGearFactor, Time.deltaTime * 5f);
+
+        var gearNumFactor = currentGear / (float)numGears;
+        rpm = Mathf.Lerp(gearNumFactor, 1, currentGearPerc);
+
+        float speedPercentage = Mathf.Abs(currentSpeed / maxSpeed);
+        float upperGearMax = (1 / (float)numGears) * (currentGear + 1);
+        float downGearMax = (1 / (float)numGears) * (currentGear);
+
+        if (currentGear > 0 && speedPercentage < downGearMax)
+            currentGear--;
+        if(speedPercentage > upperGearMax && (currentGear < (numGears - 1)))
+            currentGear++;
+
+        float pitch = Mathf.Lerp(lowPitch, highPitch, rpm);
+        highAccel.pitch = Mathf.Min(highPitch, pitch) * 0.25f;
+    }
+
+    public void Go(float accel, float steer, float brake)
     {
         accel = Mathf.Clamp(accel, -1, 1);
-        float thrustTorque = accel * torque;
+        steer = Mathf.Clamp(steer, -1, 1) * maxSteerAngle;
+        brake = Mathf.Clamp(brake, 0, 1) * maxBrakeTorque;
 
-        WC.motorTorque = thrustTorque;
+        if(brake != 0)
+        {
+            brakeLightRight.SetActive(true);
+            brakeLightLeft.SetActive(true);
+        }
+        else
+        {
+            brakeLightRight.SetActive(false);
+            brakeLightLeft.SetActive(false);
+        }
 
-        Quaternion quat;
-        Vector3 position;
-        WC.GetWorldPose(out position, out quat);
-        this.transform.position = position;
-        this.transform.rotation = quat;
+        float thrustTorque = 0;
+
+        if(currentSpeed < maxSpeed)
+            thrustTorque = accel * torque;
+
+        for(int i = 0; i < 4; i++)
+        {
+            WCs[i].motorTorque = thrustTorque;
+
+            if (i < 2)
+                WCs[i].steerAngle = steer;
+            else
+                WCs[i].brakeTorque = brake;
+
+            Quaternion quat;
+            Vector3 position;
+            WCs[i].GetWorldPose(out position, out quat); 
+            Wheels[i].transform.position = position;
+            Wheels[i].transform.rotation = quat;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CheckForSkid()
     {
-        float a = Input.GetAxis("Vertical");
-        Go(a);
+        int numSkidding = 0;
+
+        for(int i = 0; i < 4; i++)
+        {
+            WheelHit wheelHit;
+            WCs[i].GetGroundHit(out wheelHit);
+
+
+            if(Mathf.Abs(wheelHit.forwardSlip) >= 0.4f || Mathf.Abs(wheelHit.sidewaysSlip) >= 0.4f)
+            {
+                numSkidding++;
+
+                if(!skidSound.isPlaying)
+                {
+                    skidSound.Play();
+                }
+                // StartSkidTrail(i);
+                skidSmoke[i].transform.position = WCs[i].transform.position - WCs[i].transform.up * WCs[i].radius;
+                skidSmoke[i].Emit(1);
+            }
+            else
+            {
+                // EndSkidTrail(i);
+            }
+        }
+
+        if(numSkidding == 0 && skidSound.isPlaying)
+        {
+            skidSound.Stop();
+        }
     }
+
+    
 }
